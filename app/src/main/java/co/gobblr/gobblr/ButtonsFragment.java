ad1e248fc8e8f1fecc5e8924d398d31c35276ec7
BriungRi <brnli7@gmail.com>
2015-09-04 21:45:56 -0400

    Added OnClickListener to Fragment

--- app/src/main/java/co/gobblr/gobblr/ButtonsFragment.java
+++ app/src/main/java/co/gobblr/gobblr/ButtonsFragment.java
@@ -6,16 +6,45 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 
 /**
  * Created by Brian on 9/4/2015.
  */
-public class ButtonsFragment extends Fragment {
+public class ButtonsFragment extends Fragment implements View.OnClickListener {
+
+    Button bHome;
+    Button bProfile;
+    Button bSettings;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.buttons_fragment, container, false);
+
+        bHome = (Button) view.findViewById(R.id.bHome);
+        bProfile = (Button) view.findViewById(R.id.bProfile);
+        bSettings = (Button) view.findViewById(R.id.bSettings);
+
+        bHome.setOnClickListener(this);
+        bProfile.setOnClickListener(this);
+        bSettings.setOnClickListener(this);
+
         return view;
     }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case (R.id.bHome):
+                break;
+            case (R.id.bProfile):
+                break;
+            case (R.id.bSettings):
+                break;
+        }
+    }
 }
+
