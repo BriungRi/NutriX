4bff6f1214854aa00f839c7bef1756df7c8f5f05
BriungRi <brnli7@gmail.com>
2015-09-04 21:36:29 -0400

    Fragment added

---
+++ app/src/main/java/co/gobblr/gobblr/ButtonsFragment.java
@@ -0,0 +1,21 @@
+package co.gobblr.gobblr;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Brian on 9/4/2015.
+ */
+public class ButtonsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.buttons_fragment, container, false);
+        return view;
+    }
+}
